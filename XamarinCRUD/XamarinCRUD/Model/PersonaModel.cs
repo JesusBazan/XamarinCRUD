d41a04608dc5e0d43c91bde89f81303c0018888f
EmmanuelBazan <mqkz99@gmail.com>
2021-05-30 22:38:29 -0500

    Add project files.

---
+++ XamarinCRUD/XamarinCRUD/Model/PersonaModel.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace XamarinCRUD.Model
+{
+    public class PersonaModel:INotifyPropertyChanged
+    {
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void OnPropertyChanged([CallerMemberName] String nombre = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nombre));
+        }
+
+        private String id;
+
+        public String Id
+        {
+            get { return id; }
+            set { id = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private String nombre;
+
+        public String Nombre
+        {
+            get { return nombre; }
+            set { nombre = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(NombreCompleto));
+            }
+        }
+
+        private bool isBusy = false;
+
+        public bool IsBusy
+        {
+            get { return isBusy = false; }
+            set { isBusy = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        private String apellido;
+
+        public String Apellido
+        {
+            get { return apellido; }
+            set { apellido = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(NombreCompleto));
+            }
+        }
+
+        private int edad;
+
+        public int Edad
+        {
+            get { return edad; }
+            set { edad = value;
+                OnPropertyChanged();
+            }
+        }
+
+        private String nombreCompleto;
+
+        public String NombreCompleto
+        {
+            get { return $"{Nombre}{Apellido}"; }
+            set { nombreCompleto = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(Mensaje));
+            }
+        }
+
+        private String mensaje;
+
+        public String Mensaje
+        {
+            get { return $"Tu nombre completo es {NombreCompleto}"; }
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ XamarinCRUD/XamarinCRUD/View/DetallePage.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using XamarinCRUD.Model;
+
+namespace XamarinCRUD.View
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class DetallePage : ContentPage
+    {
+        public DetallePage(PersonaModel modelo)
+        {
+            InitializeComponent();
+            BindingContext = modelo;
+        }
+    }
+}
